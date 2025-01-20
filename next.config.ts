8cb9757e22411c110ee5de110f27cf3ec39f8c52
abdel hamed <vfor12@yahoo.com>
2025-01-20 02:20:03 +0200

    abdel hamed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true, // تفعيل الوضع الصارم
   images: {
-    domains: ['plus.unsplash.com','i.pinimg.com','images.unsplash.com','images.pexels.com'],    // السماح بتحميل الصور من هذا المضيف
+    domains: ['res.cloudinary.com'],    // السماح بتحميل الصور من هذا المضيف
   },
 };
 
