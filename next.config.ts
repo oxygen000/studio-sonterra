ba1c0f54693f8b3d615ede61bd2e6a29036638f3
Abdel Hamed Reda <101983081+oxygen000@users.noreply.github.com>
2025-01-20 02:30:36 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true, // تفعيل الوضع الصارم
   images: {
-    domains: ['res.cloudinary.com'],    // السماح بتحميل الصور من هذا المضيف
+    domains: ['i.imgur.com'],    // السماح بتحميل الصور من هذا المضيف
   },
 };
 
